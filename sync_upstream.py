d973b2b34c67aa03060a5c8c5529dcef507eee32
november-pain <max.chikita@gmail.com>
2022-04-07 12:06:53 +0000

    remove dangling branches

--- sync_upstream.py
+++ sync_upstream.py
@@ -24,10 +24,13 @@ upstream_branches = [branch.name.split('/', 1)[1] for branch in remote_upstream_
 common_branches = [branch for branch in upstream_branches if branch in origin_branches]
 new_branches = [branch for branch in upstream_branches if branch not in origin_branches]
 
+dangling_branches = [branch for branch in origin_branches not in upstream_branches]
+
 print("Upstream branches: ", upstream_branches)
 print("Origin branches: ", origin_branches)
 print("New branches: ", new_branches)
 print("Common branches: ", common_branches)
+print("Dangling branches: ", dangling_branches)
 
 def merge_existing_branch(git, branch, main_branch):
     try:
@@ -55,13 +58,20 @@ def merge_non_existing_branch(git, branch, main_branch):
     else:
         print("*"*20 + "-Successfuly merged " + branch + " branch-" + "*"*20)
 
+def delete_branch(git, branch):
+    git.branch('-D', branch)
+    git.push()
 
 git = repo.git
 
-print("*"*20 + "-Syncing common branches-" + "*"*20)
+print("\n" + "*"*20 + "-Syncing common branches-" + "*"*20)
 for branch in common_branches:
     merge_existing_branch(git, branch, main_branch)
 
-print("*"*20 + "-Syncing new branches-" + "*"*20)
+print("\n" + "*"*20 + "-Syncing new branches-" + "*"*20)
 for branch in new_branches:
     merge_non_existing_branch(git, branch, main_branch)
+
+print("\n" + "*"*20 + "-Deleting old dangling branches-" + "*"*20)
+for branch in dangling_branches:
+    delete_branch(branch)
